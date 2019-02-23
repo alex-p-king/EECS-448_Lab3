7aae424666b5888e4c2dd1f5bde086511b08f823
Alex King <ktmking611@gmail.com>
2019-02-22 18:07:19 -0600

    updated exercise 2 to have images, previous and next buttons, and maintain a counter of what slide should be displayed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Exercise 1
 function validate(){
     let t1 = document.getElementById("enter-text-field").value;
     let t2 = document.getElementById("verify-text-field").value;
@@ -8,5 +9,30 @@ function validate(){
     }
     else{
         alert("Passwords do not match")
+    }
+}
+
+
+//Exercise 2
+let slideNum = 1;
+
+function lastSlide(){
+    if(slideNum == 1){
+        slideNum = 5;
+        console.log(slideNum);
+    }
+    else{
+        slideNum = slideNum - 1;
+        console.log(slideNum);
+    }
+}
+function nextSlide(){
+    if(slideNum === 5){
+        slideNum = 1;
+        console.log(slideNum);
+    }
+    else{
+        slideNum++;
+        console.log(slideNum);
     }
 }
